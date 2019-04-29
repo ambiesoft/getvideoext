8993af27eb2137e94891bb7bf4b6e8607e3a7a5e
ambiesoft <ambiesoft.trueff@gmail.com>
2019-04-29 22:43:13 +0900

    start

---
+++ getvideoext/getvideoext.cpp
@@ -0,0 +1,37 @@
+﻿// getvideoext.cpp : Defines the entry point for the application.
+//
+
+#include "getvideoext.h"
+#include "CommandLineParser.h"
+#include "RunCommandGetResult.h"
+
+using namespace std;
+using namespace Ambiesoft;
+
+int main(int argc, const char* argv[])
+{
+	CCommandLineParser parser;
+
+	string ffprobe = "ffprobe";
+	parser.AddOption("-f", 1, &ffprobe);
+
+	parser.Parse(argc, argv);
+
+	DWORD nRet;
+	string output, err;
+	DWORD dwLE;
+	RunCommandGetResult(
+		ffprobe.c_str(),
+		"-h",
+		&nRet,
+		&output,
+		&err,
+		&dwLE);
+	
+	if (nRet == 0)
+		cout << output << endl;
+	else
+		cerr << err << endl;
+
+	return 0;
+}
